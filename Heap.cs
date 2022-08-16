84fe8f74a161481fecd20cbbc230c9eb3e1d81c2
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-16 15:40:31 -0300

    Adicionando algumas funções básicas do Heap

--- Heap.cs
+++ Heap.cs
@@ -8,6 +8,85 @@ namespace Heap
 {
     internal class Heap
     {
+        internal class Element
+        {
+            public object value { get; set; }
+            public int priority { get; set; }
+            public Element(object value, int priority)
+            {
+                this.value = value;
+                this.priority = priority;
+            }
 
+            public override string ToString(){
+                return this.value.ToString() + ":" + this.priority.ToString();
+            }
+        }
+        
+        private Element[] array { get; set; }
+        public int size { get; set; }
+        private int lastElement { get; set; }
+        public Heap(int lenght)
+        {
+            this.size = 0;
+            this.array = new Element[lenght];
+        }
+
+        public bool IsEmpty()
+        {
+            return this.size == 0;
+        }
+        private bool IsFull()
+        {
+            return this.size == this.array.Length;
+        }
+        private void DoubleArray()
+        {
+            Element[] newArray = new Element[this.array.Length * 2];
+            Array.Copy(this.array, newArray, this.array.Length);
+            this.array = newArray;
+        }
+        public void Insert(object value, int priority)
+        {
+            if (IsFull())
+            {
+                DoubleArray();
+            }
+            this.size++;
+            this.array[this.size] = new Element(value, priority);
+            //UpHeap();
+        }
+
+        public object RemoveMin()
+        {
+            return 0;
+        }
+        public object Min()
+        {
+            return this.array[0];
+        }
+
+        private void UpHeap(int priority)
+        {
+
+        }
+        private void DownHeap(int priority)
+        {
+
+        }
+        private void Swap(int priority, int parent)
+        {
+            Element temp = this.array[priority];
+            this.array[priority] = this.array[parent];
+            this.array[parent] = temp;
+        }
+        
+        public void Print()
+        {
+            for (int i = 0; i < this.size; i++)
+            {
+                Console.WriteLine(this.array[i].value);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Heap.Heap heap = new(10);
+
+heap.Print();
+heap.Insert("elemento1", 5);
+heap.Print();
+heap.Insert("elemento2", 7);
+heap.Print();
+heap.Insert("elemento2", 4);
+heap.Print();
